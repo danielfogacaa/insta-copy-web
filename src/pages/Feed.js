451ccc7dea7ec8de165c2f9cf17ffe8e691671b7
unknown <daniel.fogaca.75@hotmail.com>
2023-01-04 21:04:34 -0300

    refactor: get feed info from backend

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "../services/api";
 
 import "./Feed.css";
 import more from "../assests/more.svg";
@@ -6,57 +8,51 @@ import like from "../assests/like.svg";
 import comment from "../assests/comment.svg";
 import send from "../assests/send.svg";
 
-class Feed extends Component {
-  render() {
-    return (
-      <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Daniel Fogaça</span>
-              <span className="place">Cerquilho</span>
-            </div>
-            <img src={more} alt="Mais" />
-          </header>
-          <img src="http://localhost:3333/files/galax.jpg" alt="Mais" />
-          <footer>
-            <div className="actions">
-              <img src={like} alt="Like" />
-              <img src={comment} alt="Comentar" />
-              <img src={send} alt="Enviar" />
-            </div>
-            <strong>900 curtidas</strong>
-            <p>
-              Um post muito massa
-              <span>#react #omnistack #top</span>
-            </p>
-          </footer>
-        </article>
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Daniel Fogaça</span>
-              <span className="place">Cerquilho</span>
-            </div>
-            <img src={more} alt="Mais" />
-          </header>
-          <img src="http://localhost:3333/files/galax.jpg" alt="Mais" />
-          <footer>
-            <div className="actions">
-              <img src={like} alt="Like" />
-              <img src={comment} alt="Comentar" />
-              <img src={send} alt="Enviar" />
-            </div>
-            <strong>900 curtidas</strong>
-            <p>
-              Um post muito massa
-              <span>#react #omnistack #top</span>
-            </p>
-          </footer>
-        </article>
-      </section>
-    );
-  }
+function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  const getResponse = async () => {
+    const response = await api.get("posts");
+    const data = response.data;
+    setPosts(data);
+    return data;
+  };
+
+  useEffect(() => {
+    getResponse();
+  }, []);
+
+  console.log("p", posts);
+
+  return (
+    <section id="post-list">
+      {posts.length &&
+        posts.map((post) => (
+          <article>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
+              <img src={more} alt="Mais" />
+            </header>
+            <img src={`http://localhost:3333/files/${post.image}`} alt="Mais" />
+            <footer>
+              <div className="actions">
+                <img src={like} alt="Like" />
+                <img src={comment} alt="Comentar" />
+                <img src={send} alt="Enviar" />
+              </div>
+              <strong>{`${post.likes} curtidas`}</strong>
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
+    </section>
+  );
 }
 
 export default Feed;
